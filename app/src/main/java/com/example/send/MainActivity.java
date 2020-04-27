2612f1ed6a6641b5c831b1e8cbb5a02279da76bf
brokkoli71 <brokkoli71b@gmail.com>
2020-04-28 00:51:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/send/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.send;
+
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        button.setText(ip);
+    }
+
+    public void send(View v){
+        MessageSender messageSender = new MessageSender();
+        messageSender.execute(editText.getText().toString());
+    }
+}
