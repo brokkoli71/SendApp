217c747cb7d05f0f8de88b44beb23e7b3886a2bb
brokkoli71 <brokkoli71b@gmail.com>
2020-05-01 00:14:12 +0200

    pick image. not tested

--- app/src/main/java/com/example/send/MainActivity.java
+++ app/src/main/java/com/example/send/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.send;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,14 +12,17 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +36,7 @@ import java.net.Socket;
 public class MainActivity extends AppCompatActivity {
 
     EditText e1, e2;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +56,20 @@ public class MainActivity extends AppCompatActivity {
         e1 = findViewById(R.id.editText);
         e2 = findViewById(R.id.editText2);
         Button b = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView);
+
+
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+            }
+        });
+
         TextView textView = findViewById(R.id.textView);
 
         WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
@@ -55,6 +78,41 @@ public class MainActivity extends AppCompatActivity {
 
         Thread myThread = new Thread(new MyServer());
         myThread.start();
+    }
+
+    @Override //https://medium.com/@hasangi/capture-image-or-choose-from-gallery-photos-implementation-for-android-a5ca59bc6883
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage =  data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+            }
+        }
+
     }
 
     class MyServer implements  Runnable{
@@ -92,7 +150,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     public void send(View v){
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
         BackgroundTask backgroundTask = new BackgroundTask();
         backgroundTask.execute(e1.getText().toString(), e2.getText().toString());
     }
