1c237d668133dcc57e91d5f77371117b544629b0
brokkoli71 <brokkoli71b@gmail.com>
2020-05-09 00:59:59 +0200

    start working on Encryptor and further gathering of ideas for UI

---
+++ app/src/main/java/com/example/send/Encryptor.java
@@ -0,0 +1,23 @@
+package com.example.send;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class Encryptor {
+
+    Encryptor(){
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128); // 192 and 256 bits may not be available
+            SecretKey secretKey = keyGenerator.generateKey();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
